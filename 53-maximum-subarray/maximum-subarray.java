c53a833f277d4cd0596a2c50913c2454033dc8ff
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-09-18 17:35:51 +0530

    Time: 1 ms (99.93%) | Memory: 77.4 MB (20.14%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int curr_sum = Integer.MIN_VALUE;
+        for(int i : nums){
+            if(curr_sum <= 0) curr_sum = i;
+            else {
+                curr_sum += i;
+            } 
+            max = max<curr_sum ? curr_sum : max;
+        }
+        return max;
+    }
+}
